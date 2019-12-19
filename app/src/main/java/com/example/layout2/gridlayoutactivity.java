c7bc042db6ab00d75a6585450d2d3b5903a6483c
waityoucandothat <ponce.carlos@uabc.edu.mx>
2019-12-18 21:54:47 -0800

    primer algo

---
+++ app/src/main/java/com/example/layout2/gridlayoutactivity.java
@@ -0,0 +1,14 @@
+package com.example.layout2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gridlayoutactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridlayoutactivity);
+    }
+}
